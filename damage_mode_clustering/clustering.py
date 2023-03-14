df72e849c3bfea84b645a1b244ebd29a2bb1928d
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-03-14 12:58:22 +0100

    filtering works

--- damage_mode_clustering/clustering.py
+++ damage_mode_clustering/clustering.py
@@ -1,7 +1,38 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import data_import as di
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
 # import vallenae as ae
 
-test_pridb = di.getPrimaryDatabase("TEST")
-print(test_pridb.read_hits())
+sortby = "energy"
+epsilon = 2
+
+test_pridb = di.getPrimaryDatabase("TEST").read_hits()
+hitsno = [len(test_pridb.loc[test_pridb['channel'] == i]) for i in range(1, 8+1)]
+print(hitsno)
+test_pridb = test_pridb.loc[test_pridb['channel'] == 4]
+test_pridb.reset_index(drop=False, inplace=True)
+print(test_pridb[:])
+i = 0
+while i < len(test_pridb)-1:
+    print(i)
+    if test_pridb.loc[i+1, 'time'] - test_pridb.loc[i, 'time'] < epsilon:
+        if test_pridb.loc[i+1, 'energy'] > test_pridb.loc[i, 'energy']:
+            test_pridb.drop(i, inplace=True)
+            test_pridb.reset_index(drop=True, inplace=True)
+        else:
+            test_pridb.drop(i+1, inplace=True)
+            test_pridb.reset_index(drop=True, inplace=True)
+    else:
+        i+=1
+
+print(test_pridb[:])
+x = np.arange(1, len(test_pridb)+1, 1)
+y = test_pridb.sort_values(sortby, axis=0, ascending=False)[sortby].to_numpy()
+yp = [y[i+1]-y[i] for i in range(len(y)-1)]
+plt.plot(x, y)
+# plt.plot(x[:-1], yp)
+plt.plot([36, 36], [0, max(y)], '--')
+plt.show()
