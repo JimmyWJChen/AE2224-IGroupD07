d45bde0715e976b6a0f77cbd402a2dd09d3c28ad
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-03-14 10:27:16 +0100

    sasasasaassvdvdsds

--- Damage mode clustering/AE clustering.py
+++ Damage mode clustering/AE clustering.py
@@ -1,1 +1,7 @@
-print('chuj')
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import data_import as di
+# import vallenae as ae
+
+test_pridb = di.getPrimaryDatabase("TEST")
+print(test_pridb.read_hits())
--- data_import.py
+++ data_import.py
@@ -5,11 +5,11 @@ import vallenae as vae
 
 def getPrimaryDatabase(label, testno=1):
     if label == "PCLO" or label == "PCLS":
-        path = "Testing data/PLB-4-channels/PLBS4_CP090_" + label + str(testno) + ".pridb"
+        path = "Testing_data/PLB-4-channels/PLBS4_CP090_" + label + str(testno) + ".pridb"
     elif label == "TEST":
-        path = "Testing data/PLB-8-channels/PLBS8_QI090_" + label + ".pridb"
+        path = "Testing_data/PLB-8-channels/PLBS8_QI090_" + label + ".pridb"
     else:
-        path = "Testing data/PLB-8-channels/PLBS8_QI090_" + label + str(testno) + ".pridb"
+        path = "Testing_data/PLB-8-channels/PLBS8_QI090_" + label + str(testno) + ".pridb"
     HERE = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
     PRIDB = os.path.join(HERE, path)
     # print(PRIDB)
@@ -18,11 +18,11 @@ def getPrimaryDatabase(label, testno=1):
 
 def getWaveform(label, testno=1, trai=1):
     if label == "PCLO" or label == "PCLS":
-        path = "Testing data/PLB-4-channels/PLBS4_CP090_" + label + str(testno) + ".tradb"
+        path = "Testing_data/PLB-4-channels/PLBS4_CP090_" + label + str(testno) + ".tradb"
     elif label == "TEST":
-        path = "Testing data/PLB-8-channels/PLBS8_QI090_" + label + ".tradb"
+        path = "Testing_data/PLB-8-channels/PLBS8_QI090_" + label + ".tradb"
     else:
-        path = "Testing data/PLB-8-channels/PLBS8_QI090_" + label + str(testno) + ".tradb"
+        path = "Testing_data/PLB-8-channels/PLBS8_QI090_" + label + str(testno) + ".tradb"
     HERE = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
     TRADB = os.path.join(HERE, path)
     with vae.io.TraDatabase(TRADB) as tradb:
