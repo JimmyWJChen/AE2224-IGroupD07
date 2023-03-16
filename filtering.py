668d5ba7e0f9c3e3744096f9eabd5a5492e675e6
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-03-16 14:52:19 +0100

    filtering almost done

--- filtering.py
+++ filtering.py
@@ -1,7 +1,7 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import data_import as di
-import pandas
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 # import vallenae as ae
@@ -10,33 +10,50 @@ import matplotlib.pyplot as plt
 
 # standard threshold for AE 34dB, we have at 45dB -> thus missing IDs in first column -> ignore it
 
-sortby = "signal_strength"
-epsilon = 0.001
+sortby = "counts"
+epsilon = 0.01
 
 test_pridb = di.getPrimaryDatabase("TEST").read_hits()
+test_pridb = test_pridb[test_pridb['amplitude'] >= 0.005]
+test_pridb = test_pridb[test_pridb['duration'] >= 0.001]
+test_pridb = test_pridb[test_pridb['energy'] >= 1e5]
+test_pridb = test_pridb[test_pridb['signal_strength'] >= 2000]
+test_pridb = test_pridb[test_pridb['counts'] >= 50]
+
 hitsno = [len(test_pridb.loc[test_pridb['channel'] == i]) for i in range(1, 8+1)]
 print(hitsno)
-test_pridb = test_pridb.loc[test_pridb['channel'] == 4]
-test_pridb.reset_index(drop=False, inplace=True)
-print(test_pridb[:])
-i = 0
-while i < len(test_pridb)-1:
-    print(i)
-    if test_pridb.loc[i+1, 'time'] - test_pridb.loc[i, 'time'] < epsilon:
-        if test_pridb.loc[i+1, 'energy'] > test_pridb.loc[i, 'energy']:
-            test_pridb.drop(i, inplace=True)
-            test_pridb.reset_index(drop=True, inplace=True)
+test_pridb_channels = []
+for channel in range(1, 8+1):
+    test_pridb_chan = test_pridb.loc[test_pridb['channel'] == channel].copy()
+    test_pridb_chan.reset_index(drop=False, inplace=True)
+    i = 0
+    while i < len(test_pridb_chan)-1:
+        if test_pridb_chan.loc[i+1, 'time'] - test_pridb_chan.loc[i, 'time'] < epsilon:
+            if test_pridb_chan.loc[i+1, 'energy'] > test_pridb_chan.loc[i, 'energy']:
+                test_pridb_chan.drop(i, inplace=True)
+                test_pridb_chan.reset_index(drop=True, inplace=True)
+            else:
+                test_pridb_chan.drop(i+1, inplace=True)
+                test_pridb_chan.reset_index(drop=True, inplace=True)
         else:
-            test_pridb.drop(i+1, inplace=True)
-            test_pridb.reset_index(drop=True, inplace=True)
-    else:
-        i+=1
+            i+=1
+    # test_pridb_chan.reset_index(drop=True, inplace=True)
+    test_pridb_channels.append(test_pridb_chan)
 
-print(test_pridb[:])
-x = np.arange(1, len(test_pridb)+1, 1)
-y = test_pridb.sort_values(sortby, axis=0, ascending=False)[sortby].to_numpy()
-yp = [y[i+1]-y[i] for i in range(len(y)-1)]
-plt.plot(x, y)
-# plt.plot(x[:-1], yp)
-plt.plot([36, 36], [0, max(y)], '--')
-plt.show()
+# print(test_pridb_channels)
+test_pridb_output = pd.concat(test_pridb_channels, ignore_index=True)
+print(test_pridb_output)
+hitsno = [len(test_pridb_output.loc[test_pridb_output['channel'] == i]) for i in range(1, 8+1)]
+print(hitsno)
+
+# chan_to_plot = 1
+# chan_to_plot -=1
+
+# x = np.arange(1, len(test_pridb_channels[chan_to_plot])+1, 1)
+# y = test_pridb_channels[chan_to_plot].sort_values(sortby, axis=0, ascending=False)[sortby].to_numpy()
+# # yp = [y[i+1]-y[i] for i in range(len(y)-1)]
+# plt.plot(x, y)
+# plt.yscale('linear')
+# # plt.plot(x[:-1], yp)
+# plt.plot([36, 36], [0, max(y)], '--')
+# plt.show()
