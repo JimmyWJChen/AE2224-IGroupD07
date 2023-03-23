39d23baadf1e1d076c153c1e1b471901e1090144
Gordonljp <Gordonljp@gmail.com>
2023-03-23 16:03:15 +0100

    changed dispersion file

--- Dispersion_Curves/Dispersion.py
+++ Dispersion_Curves/Dispersion.py
@@ -6,13 +6,17 @@ import csv
 Frequency = []
 Velocity = []
 with open('A0.csv', newline='') as A0:
-    Data = csv.reader(A0, delimiter=' ')
-Size = len(Data)
-Frequency= np.zero(Size)
-Velocity = np.zero(Size)
-for i in Size:
-    Frequency[i], Velocity[i] = Data.split(',')
-    Frequency[i], Velocity[i] = float(Frequency[i]), float(Velocity[i])
+    Data = csv.reader(A0)
+#Size = len(Data)
+#Frequency= np.zero(Size)
+#Velocity = np.zero(Size)
+    for row in Data:
+        Frequency.append(row[0])
+        Velocity.append(row[1])
 
+for i in range(0, len(Frequency)):
+        Frequency[i], Velocity[i] = float(Frequency[i]), float(Velocity[i])
+
+fA0 = sp.interpolate.interp1d(Frequency, Velocity, kind="linear", fill_value="extrapolate")
 plt.plot(Frequency, Velocity)
 plt.show()
