9209a965f14aa61b28017acbe395ec34af2cd368
LuukDevine <104198426+LuukDevine@users.noreply.github.com>
2023-03-23 17:05:30 +0100

    nothing drastic

--- Dispersion_Curves/Dispersion.py
+++ Dispersion_Curves/Dispersion.py
@@ -1,5 +1,4 @@
-import scipy
-from scipy import interpolate
+import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -19,32 +18,28 @@ with open('A0.csv', newline='') as A0:
 for i in range(0, len(Frequency)):
         Frequency[i], Velocity[i] = float(Frequency[i]), float(Velocity[i])
 
-fA0 = scipy.interpolate.interp1d(Frequency, Velocity, kind="linear", fill_value="extrapolate")
+fA0 = sp.interpolate.interp1d(Frequency, Velocity, kind="linear", fill_value="extrapolate")
 
 #Symmetric Below
 Velocity1 = []
 Frequency1=[]
 with open('S0.csv', newline='') as S0:
-    Data1 = csv.reader(S0)
+    Data1 = csv.reader(A0)
 #Size = len(Data)
 #Frequency= np.zero(Size)
 #Velocity = np.zero(Size)
-    for row in Data1:
+    for row in Data:
         Frequency1.append(row[0])
         Velocity1.append(row[1])
 
 for i in range(0, len(Frequency1)):
         Frequency1[i], Velocity1[i] = float(Frequency1[i]), float(Velocity1[i])
 
-fS0 = scipy.interpolate.interp1d(Frequency1, Velocity1, kind="linear", fill_value="extrapolate")
-print(fA0(200000))
-plt.title('Dispersion')
-plt.plot(Frequency, Velocity,label = 'Asymmetric')
-plt.plot(Frequency1, Velocity1, label = 'Symmetric')
-plt.ylim(0,8000)
-plt.legend()
+fA0 = sp.interpolate.interp1d(Frequency1, Velocity1, kind="linear", fill_value="extrapolate")
+
+plt.title('Asymmetric')
+plt.plot(Frequency, Velocity)
 plt.show()
 plt.title('Symmetric')
 plt.plot(Frequency1, Velocity1)
-plt.ylim(0,8000)
-#plt.show()
+plt.show()
