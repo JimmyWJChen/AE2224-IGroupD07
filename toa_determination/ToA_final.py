ae7aa4df97ec5dff840dac230088f068999d05ee
RobBouwmeester <125354287+RobBouwmeester@users.noreply.github.com>
2023-03-30 17:30:42 +0200

    upgrade file with extra features

--- toa_determination/ToA_final.py
+++ toa_determination/ToA_final.py
@@ -4,27 +4,19 @@ import data_import as di
 import vallenae as vae
 import numpy as np
 
-
-def check_channels(file_name, time_lst, n_sensors):
-    #check if the channel number corresponds correctly to the assigned column of a time
-    
-    length = len(time_lst)
-    n_hits = int(length / n_sensors)
-    if length % n_sensors == 0:
-        
-        for i in range(n_sensors):
-            for j in range(n_hits):
-                
-                if time_lst[i * n_hits + j, 1] != i+1:
-                    print("Channel number of file: " + file_name + " does not match")
-
-
-def get_toa_filtered(file_name, n_sensors):
+def get_toa_filtered(label, testno):
     #get trai values from pridb, get the tribd file of these trai values and calculate the time difference
     #to add the time difference to the time in the pridb file
+    if label == "PCLO" or label == "PCLS":
+        path = "testing_data/PLB-4-channels/PLBS4_CP090_" + label + str(testno) + ".pridb"
+        n_sensors = 4
+    elif label == "TEST":
+        path = "testing_data/PLB-8-channels/PLBS8_QI090_" + label + ".pridb"
+        n_sensors = 8
+    else:
+        path = "testing_data/PLB-8-channels/PLBS8_QI090_" + label + str(testno) + ".pridb"
+        n_sensors = 8
     
-    label = file_name.split("_")[-1][:-7]
-    testno = file_name[-7]
     filtered_pridb = di.filterPrimaryDatabase(di.getPrimaryDatabase(label, testno), label, testno)
     
     trai_lst = filtered_pridb.iloc[:, -1:].to_numpy()
@@ -36,19 +28,17 @@ def get_toa_filtered(file_name, n_sensors):
         print("In file: " + file_name + " the number of signals is not divisble by 4")
         return None
     
-    channel_lst = np.zeros((n_values, 1))
     for i, trai in enumerate(trai_lst):
         y,t = di.getWaveform(label, testno, int(trai))
         hc_index = vae.timepicker.hinkley(y, alpha=5)[1]
         time_difference = t[hc_index]
         time_lst[i][0] = time_lst[i][0] + time_difference
-        channel_lst[i] = time_lst[i][1]
-        
-    check_channels(file_name, time_lst, n_sensors)
-    
 
+    return time_lst[;,0]
+
+def reshaping(time_lst):
+    
     new_times = np.reshape(time_lst[:,0], (n_sensors, int(n_values/n_sensors)))
-    new_channels = np.reshape(channel_lst, (n_sensors, int(n_values/n_sensors)))
     
     return np.transpose(new_times)
 
@@ -62,8 +52,11 @@ def get_toa_plb(n_sensors):
     for file in plb_files:
         if file[-3:] == "idb":
             if file != "PLBS8_QI090_ST1.pridb":
-                toa_array = get_toa_filtered(file, n_sensors)
-                np.savetxt(f"AE2224-IGroupD07\\testing_data\\toa_improved\PLB-{n_sensors}-channels\{file[:-5]}csv", toa_array, delimiter=",")
+                
+                label = file.split("_")[-1][:-7]
+                testno = file[-7]
+                toa_array = get_toa_filtered(label, testno)
+                #np.savetxt(f"AE2224-IGroupD07\\testing_data\\toa_improved\PLB-{n_sensors}-channels\{file[:-5]}csv", toa_array, delimiter=",")
             
 get_toa_plb(4)
     
