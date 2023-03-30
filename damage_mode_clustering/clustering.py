d0da25b52209c421504a857e06eee696172e6347
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-03-30 15:34:56 +0200

    clustering kmeans, hierarchical done!

--- damage_mode_clustering/clustering.py
+++ damage_mode_clustering/clustering.py
@@ -30,48 +30,24 @@ def PCA(datapoints, n):
     var = usedValues/totalVariance
     return X_pca, var
 
-def train_kmeans(x_train, n_clusters):
-    x_train = np.sort(x_train.reshape(-1))
-    cluster_model = KMeans(
-        n_clusters=n_clusters, n_init=10).fit(
-        x_train.reshape(-1, 1))
-    _, indx = np.unique(cluster_model.labels_, return_index=True)
-    lookup_labels = [cluster_model.labels_[i] for i in sorted(indx)]
-    return cluster_model, lookup_labels, n_clusters
+def clustering_kmeans(x_train, n_clusters):
+    cluster_model = KMeans(n_clusters=n_clusters, n_init=10).fit(x_train)
+    x_train['cluster'] = cluster_model.predict(x_train)
+    return x_train
 
-def train_hierarchical(x_train, n_clusters, linkage='ward'):
-    x_train = np.sort(x_train.reshape(-1))
-    cluster_model = AgglomerativeClustering(
-        n_clusters=n_clusters, linkage=linkage).fit(
-        x_train.reshape(-1, 1))
-    _, indx = np.unique(cluster_model.labels_, return_index=True)
-    lookup_labels = [cluster_model.labels_[i] for i in sorted(indx)]
-    return cluster_model, lookup_labels, n_clusters
-
-def predict(cluster_model, lookup_labels, data_dict, cluster_method):
-    clusters_dict = {f'a{i + 1}': [] for i in range(6)}
-    for i in range(len(data_dict)):
-        if cluster_method == 'kmeans':
-            pred = cluster_model.predict(
-                np.array(data_dict[f'a{i + 1}']['strains']).squeeze().reshape(-1, 1))
-            labels = []
-            for j in range(len(pred)):
-                labels.append(np.where(lookup_labels == pred[j])[0])
-            clusters_dict[f'a{i + 1}'].extend(sorted(labels))
-        else:
-            labels = cluster_model.fit_predict(
-                np.array(data_dict[f'a{i + 1}']['strains']).squeeze().reshape(-1, 1))
-            clusters_dict[f'a{i + 1}'].extend(sorted(labels))
-
-    return clusters_dict
+def clustering_hierarchical(x_train, n_clusters, linkage='ward'):
+    cluster_model = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage).fit(x_train)
+    x_train['cluster'] = cluster_model.labels_
+    return x_train
 
 if __name__=="__main__":
     datapoints = di.filterPrimaryDatabase(di.getPrimaryDatabase("PCLO", 3), "PCLO", 3)
+    datapoints = di.addPeakFreq(datapoints, "PCLO", 3)
     datapoints = datapoints[datapoints['channel'] == 2]
-    X_cluster = datapoints[['amplitude', 'counts']].to_numpy()
+    X_cluster = datapoints[['amplitude', 'frequency']]
     print(X_cluster)
-    cluster_model, lookup_labels, n_clusters = train_kmeans(X_cluster, 4)
-    clusters = predict(cluster_model, lookup_labels, 0, 'kmeans')
+    print(clustering_hierarchical(X_cluster, 4))
+    # clusters = predict(cluster_model, lookup_labels, 0, 'kmeans')
     # data_compressed, var = PCA(datapoints, 2)
     # plt.scatter(data_compressed[0], data_compressed[1])
     # plt.show()
