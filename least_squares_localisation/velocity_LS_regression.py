69166119451eed931889651e03ff483bdcde360e
JimmyWJChen <W.J.Chen-1@student.tudelft.nl>
2023-04-25 14:45:16 +0200

    Commented out the import, because the file doesn't work yet.

--- least_squares_localisation/velocity_LS_regression.py
+++ least_squares_localisation/velocity_LS_regression.py
@@ -4,7 +4,7 @@ import os, sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import data_import as di
-import toa_determination.ToA_final as ToA
+#import toa_determination.ToA_final as ToA
 
 
 
