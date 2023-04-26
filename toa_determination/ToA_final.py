28d203955ec287c4eb7e887fdc321852ffd0d05f
RobBouwmeester <125354287+RobBouwmeester@users.noreply.github.com>
2023-04-26 17:27:52 +0200

    fixed issue

--- toa_determination/ToA_final.py
+++ toa_determination/ToA_final.py
@@ -4,7 +4,7 @@ import data_import as di
 import vallenae as vae
 import numpy as np
 
-def get_toa_filtered(label, testno, timepicker):
+def get_toa_filtered(label: str, testno: int, timepicker: str):
     """
     get trai values from pridb, get the tribd file of these trai values and calculate the time difference
     to add the time difference to the time in the pridb file.
@@ -13,13 +13,8 @@ def get_toa_filtered(label, testno, timepicker):
     """
     
     if label == "PCLO" or label == "PCLS":
-        path = "testing_data/PLB-4-channels/PLBS4_CP090_" + label + str(testno) + ".pridb"
         n_sensors = 4
-    elif label == "TEST":
-        path = "testing_data/PLB-8-channels/PLBS8_QI090_" + label + ".pridb"
-        n_sensors = 8
     else:
-        path = "testing_data/PLB-8-channels/PLBS8_QI090_" + label + str(testno) + ".pridb"
         n_sensors = 8
     
     filtered_pridb = di.filterPrimaryDatabase(di.getPrimaryDatabase(label, testno), label, testno)
@@ -53,9 +48,20 @@ def get_toa_filtered(label, testno, timepicker):
         time_difference = t[hc_index]
         time_lst[i][0] = time_lst[i][0] + time_difference
 
-    return time_lst[:,0], n_sensors, n_values
+    return time_lst[:,0]
 
-def reshape(time_lst, n_sensors, n_values):
+def reshape(label, testno, timepicker):
+    
+    time_lst = get_toa_filtered(label, testno, timepicker)
+    if label == "PCLO" or label == "PCLS":
+        n_sensors = 4
+    elif label == "TEST":
+        n_sensors = 8
+    else:
+        n_sensors = 8
+    filtered_pridb = di.filterPrimaryDatabase(di.getPrimaryDatabase(label, testno), label, testno)
+    trai_lst = filtered_pridb.iloc[:, -1:].to_numpy()
+    n_values = np.shape(trai_lst)[0]
     
     new_times = np.reshape(time_lst, (n_sensors, int(n_values/n_sensors)))
     
@@ -78,6 +84,8 @@ def get_toa_plb(n_sensors, timepicker):
                 
                 label = file.split("_")[-1][:-7]
                 testno = file[-7]
-                toa_array = reshape(*get_toa_filtered(label, testno, timepicker))
+                toa_array = reshape(label, testno, timepicker)
                 np.savetxt(f"testing_data\\toa\\PLB-{name}-{n_sensors}-channels\\{file[:-5]}csv", toa_array, delimiter=",")
-get_toa_plb(4,"hc")  
+
+if __name__ == "__main__":
+    get_toa_plb(4,"hc")
