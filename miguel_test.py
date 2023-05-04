ccacc0b086d7a13f08883bf5f12acc7d88dbaeab
Gordonljp <Gordonljp@gmail.com>
2023-05-04 17:23:58 +0200

    changed dispersion file

--- miguel_test.py
+++ miguel_test.py
@@ -11,12 +11,12 @@ def Velocity(fs, fa, Min, frequencies):
     v = (Min * Vsym + (100 - Min) * Vasy) / 100
     return v
 
-def non_linear(a1, a2, a3, b1, b2, b3, v, T, t1, t2, t3):
+def non_linear(a1, a2, a3, b1, b2, b3, v, t1, t2, t3):
     def equations(vars):
         x, y, T = vars
-        eq1 = x**2 + y**2 - 2*a1*x - 2*b1*y - v*T - a1**2 + b1**2 - v*t1
-        eq2 = x**2 + y**2 - 2*a2*x - 2*b2*y - v*T - a2**2 + b2**2 - v*t2
-        eq3 = x**2 + y**2 - 2*a3*x - 2*b3*y - v*T - a3**2 + b3**2 - v*t3
+        eq1 = x**2 + y**2 - 2*a1*x - 2*b1*y - v*T + a1**2 + b1**2 - v*t1
+        eq2 = x**2 + y**2 - 2*a2*x - 2*b2*y - v*T + a2**2 + b2**2 - v*t2
+        eq3 = x**2 + y**2 - 2*a3*x - 2*b3*y - v*T + a3**2 + b3**2 - v*t3
         return [eq1, eq2, eq3]
     return fsolve(equations, (1, 1, 1))
 
@@ -26,25 +26,32 @@ V = np.zeros((NoOfSens, NoOfRows))
 V = Velocity(fS0, fA0, Minimum, PeakFrequencies)
 
 
-
 file = os.path.join(os.path.dirname(__file__), "testing_data/PLB-hinkley-4-channels/PLBS4_CP090_PCLO1"+".csv")
 
 
-t2 = 1
-t3 = 1
 
 
 l_i = np.array(list(combinations([0,1,2,3,4,5,6,7], 3)))
+X = 0
+Y = 0
 
+for k in range (1):
+    for i in l_i:
+        A = []
+        B = []
+        t = []
+        for j in i:
+            A.append(SensorCoordinates[j][0])
+            B.append(SensorCoordinates[j][1])
+            t.append(TOAR[k,j])
+        x,y,T = non_linear(A[0], A[1], A[2], B[0], B[1], B[2], V[k,0], t[0], t[1], t[2])
+        #print("x=", x, "y=", y, "T=", T)
+        X += x
+        Y += y
 
-for i in l_i:
-    A = np.zeros(3)
-    B = np.zeros(3)
-    for j in i:
-        A.append(SensorCoordinates[j][0])
-        B.append(SensorCoordinates[j][1])
-    non_linear(A[0], A[1], A[2], B[0], B[1], B[2], V[i], TOAR, t2, t3)
-
+X = X/(len(l_i))
+Y = Y/(len(l_i))
+print (X, Y)
 
 #--------------------------------------------------------
 
