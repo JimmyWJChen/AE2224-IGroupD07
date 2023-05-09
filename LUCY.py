005cc327e6a02cc6a901d32b7eb14cea36dd7429
LuukDevine <l.t.dewijn@student.tudelft.nl>
2023-05-09 16:10:19 +0200

    nothing drastic

--- LUCY.py
+++ LUCY.py
@@ -1,26 +1,31 @@
 import numpy as np
-from localization_D import *
+# from localization_D import *
 from math import *
 
 X = np.array([[1, 2], [2, 2], [1, 3], [3, 4]])
-S = np.array([[[1, 2], []], [], []])
-def TOF_finder(S, X, v, i, TOAR):
-    tf = sqrt(((X[0] - S[0]) ** 2 + (X[1] - S[1]) ** 2) / v)
-    if i == 0:
+S = np.array([[[1, 2], [3, 4]], [[5, 6], [2, 4]]])
+TOAR = np.array([[1, 3], [2, 4]])
+
+
+def TOF_finder(S, X, v, i, j, k, TOAR):
+    tf = sqrt(((X[k, 0] - S[i, j, 0]) ** 2 + (X[k, 1] - S[i, j, 1]) ** 2) / v)
+    t = np.zeros(4)
+    if k == 0:
         return tf
     else:
-        t[i] = tf + TOAR[i]
-        return t[i]
+        t[k] = tf + TOAR[i, k]
+        return t[k]
 
 
-def Di_finder(S, X, i):
-    D[i] = sqrt((X[0]-S[0])**2+(X[1]-S[1])**2)
-    return D[i]
+def Di_finder(S, X, i, j, k):
+    D = sqrt((X[k, 0]-S[i, j, 0])**2+(X[k, 1]-S[i, j, 1])**2)
+    return D
 
 
-def Pi_finder(S, X, v, i, TOAR):
-    P[i] = sqrt((X[0]-S[1])**2+(X[1]-S[1])**2) + v * TOAR[i]
-    return P[i]
+def Pi_finder(S, X, v, i, j, k, TOAR):
+    P = sqrt((X[k, 0]-S[i, j, 1])**2+(X[k, 1]-S[i, j, 1])**2) + v * TOAR[i, k]
+    return P
+
 
 def LUCY(D, P):
     Y = []
@@ -29,5 +34,18 @@ def LUCY(D, P):
     LUCY = sqrt(sum(Y)/(len(D)-1))
     return LUCY
 
-for i in
+print(S)
+print(len(X))
+
+for i in range(len(S[:, 0, 0])):
+    L = []
+    for j in range(len(S[0, :, 0])):
+        D = []
+        P = []
+        for k in range(len(X)):
+            D.append(Di_finder(S,X,i,j,k))
+            P.append(Pi_finder(S,X,v,i,j,k,TOAR))
+        L.append(LUCY(D, P))
+    L_min = min(L)
+
 
