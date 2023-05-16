12281f0da48bdabd65263a6f5aa2d968053ace51
JimmyWJChen <W.J.Chen-1@student.tudelft.nl>
2023-05-16 14:41:45 +0200

    My algorithm can predict the emission sources.

--- least_squares_localisation/LS_localisation.py
+++ least_squares_localisation/LS_localisation.py
@@ -38,14 +38,14 @@ class LS_localiser():
         self.S2 = np.array([[0.030, 0.030], [0.100, 0.030], [0.040, 0.110], [0.110, 0.110]])
         self.v_LE = 4352.14708386999
         self.v_LU = 4347.18703177823
-        self.testlabels = ["PCLSR_QI00", "PCLSR_QI090", "PCLO_QI00", "PCLO_QI090"]
+        self.testlabels = ["PD_PCLSR_QI00", "PD_PCLSR_QI090", "PD_PCLO_QI00", "PD_PCLO_QI090"]
         self.channels = 4
-        self.test_label_1 = "PCLSR_QI00"
-        self.test_label_2 = "PCLSR_QI090"
-        self.test_label_3 = "PCLO_QI00"
-        self.test_label_4 = "PCLO_QI090"
+        self.test_label_1 = "PD_PCLSR_QI00"
+        self.test_label_2 = "PD_PCLSR_QI090"
+        self.test_label_3 = "PD_PCLO_QI00"
+        self.test_label_4 = "PD_PCLO_QI090"
 
-    def tau_finder(self, testlabel: str, testno: int):
+    def tau_finder(self, testlabel: str, testno: int, user: str):
         """
         For a given testlabel and testnumber return an array of ToA differences
 
@@ -55,29 +55,69 @@ class LS_localiser():
         Returns:
         dToAs (n*m) matrix
         """
-        # call ToAs
-        ToAs = reshape(testlabel, 'hc', True, testno)
-        # find number of events
-        self.events = len(ToAs)
-        # this is a 1D list, need to split it into different channels for the rest of the class
-        dToAs = np.zeros((self.events, self.channels))
-        """
-        # reshape ToA_array to have events rows and channels columns
-        ToA_array = ToA_list.reshape((self.channels, self.events))
-        # transpose it
-        ToA_array_T = ToA_array.T
-        """
-        # get the ToA differences
-        ToA_channel_1 = ToAs[:, 0]
-        for i in range(self.channels):
-            ToA_channel_i = ToAs[:, i]
-            dToA_channel_i = ToA_channel_i - ToA_channel_1
-            dToAs[:, i] = dToA_channel_i
+
+        dToAs = []
+        if user == "Jimmy":
+
+            path = "C:/Users/Jimmy Chen/OneDrive - Delft University of Technology/Documents/GitHub/AE2224-IGroupD07/testing_data/4-channels/" + str(
+                testlabel) + ".csv"
+            pridb = pd.read_csv(path)
+            # filtered_pridb = di.getPrimaryDatabase(testlabel, 1, True)
+            trai_lst = pridb.iloc[:, -2:-1].to_numpy()
+
+            time_lst = pridb.iloc[:, 1:3].to_numpy()
+            print(time_lst)
+            for i, trai in enumerate(trai_lst):
+                y, t = getWaveform(testlabel, 1, int(trai))
+
+                try:
+
+                    hc_index = vae.timepicker.hinkley(y, alpha=5)[1]
+
+
+                except:
+                    print("Invalid timepicker is chosen, hinkley is selected")
+                    hc_index = vae.timepicker.hinkley(y, alpha=5)[1]
+
+                time_difference = t[hc_index]  # type: ignore
+                time_lst[i][0] = time_lst[i][0] + time_difference
+            print(time_lst[:, 0])
+            time_array = time_lst[:, 0]
+            n_values = np.shape(trai_lst)[0]
+            new_times = np.reshape(time_array, (int(n_values / n_sensors), n_sensors))
+            print(new_times)
+            self.events = int(n_values / n_sensors)
+            dToAs = np.zeros((self.events, self.channels))
+            # get the ToA differences
+            ToA_channel_1 = new_times[:, 0]
+            for i in range(self.channels):
+                ToA_channel_i = new_times[:, i]
+                dToA_channel_i = ToA_channel_i - ToA_channel_1
+                dToAs[:, i] = dToA_channel_i
+        else:
+            # call ToAs
+            ToAs = reshape(testlabel, 'hc', True, testno)
+            # find number of events
+            self.events = len(ToAs)
+            # this is a 1D list, need to split it into different channels for the rest of the class
+            dToAs = np.zeros((self.events, self.channels))
+            """
+            # reshape ToA_array to have events rows and channels columns
+            ToA_array = ToA_list.reshape((self.channels, self.events))
+            # transpose it
+            ToA_array_T = ToA_array.T
+            """
+            # get the ToA differences
+            ToA_channel_1 = ToAs[:, 0]
+            for i in range(self.channels):
+                ToA_channel_i = ToAs[:, i]
+                dToA_channel_i = ToA_channel_i - ToA_channel_1
+                dToAs[:, i] = dToA_channel_i
 
         return dToAs
 
     # localise emission source for one test
-    def localise(self, testlabel: str, testno: int, v_type: str, X_init, iterations):
+    def localise(self, testlabel: str, testno: int, v_type: str, X_init, iterations, user):
         """
         For a given testlabel, testno, v_type,  X_init and iterations, find the emission source
 
@@ -109,7 +149,7 @@ class LS_localiser():
         else:
             raise Exception('Invalid velocity type.')
         # call dToAs
-        dToAs = self.tau_finder(testlabel, testno)
+        dToAs = self.tau_finder(testlabel, testno, user)
         # calculate location prediction for each event
         X_pred = np.zeros((self.events, 2))
         for i in range(self.events):
@@ -180,7 +220,7 @@ class LS_localiser():
         return LU, relative_LU
 
     # calculate LU of one test
-    def LU_one_test(self, testlabel: str, testno: int, v_type: str, X_init, iterations):
+    def LU_one_test(self, testlabel: str, testno: int, v_type: str, X_init, iterations, user="Jimmy"):
         """
         Takes testlabel, testno, v_type, X_init, iterations and returns array of LU and relative_LU
 
@@ -196,7 +236,7 @@ class LS_localiser():
         X_pred (n * 2 matrix)
         """
         # call X_pred, S, dToAs, v
-        X_pred, S, dToAs, v = self.localise(testlabel, testno, v_type, X_init, iterations)
+        X_pred, S, dToAs, v = self.localise(testlabel, testno, v_type, X_init, iterations, user)
         LU_array = np.zeros(self.events)
         rel_LU_array = np.zeros(self.events)
         for i in range(self.events):
@@ -205,6 +245,34 @@ class LS_localiser():
             rel_LU_array[i] = relative_LU
 
         return LU_array, rel_LU_array, X_pred, v
+# write to a csv file
+    def write_to_csv(self, X, LU, count, version, number):
+        """
+        Feed it the X array and the function will write it to a csv file
+        """
+        # create a name
+        if count == 1:
+            label = self.test_label_1
+        elif count == 2:
+            label = self.test_label_2
+        elif count == 3:
+            label = self.test_label_3
+        else:
+            label = self.test_label_4
+        name = "source_locations_backup" + label + version + str(number) + ".csv"
+        # convert LU to a matrix and transpose it
+        LU_mat = np.matrix(LU)
+        LU_T = LU_mat.T
+        # concatenate to X
+        X = np.hstack((X, LU_T))
+        # convert numpy array to pandas dataframe
+        df = pd.DataFrame(X)
+
+        # save dataframe as csv file
+        df.to_csv(name)
+        # confirm
+        print(f'X_array has successfully been saved as a csv')
+
 
 def getWaveform(label, testno=1, trai=1):
     if label[:2] == "PD":
@@ -226,6 +294,14 @@ if __name__ == "__main__":
     iterations = 50
     n_sensors = 4
     testlabel = "PD_PCLO_QI00"
+
+    #self.test_label_1 = "PCLSR_QI00"
+    #self.test_label_2 = "PCLSR_QI090"
+    #self.test_label_3 = "PCLO_QI00"
+    #self.test_label_4 = "PCLO_QI090"
+
+
+    """
     path = "C:/Users/Jimmy Chen/OneDrive - Delft University of Technology/Documents/GitHub/AE2224-IGroupD07/testing_data/4-channels/" + str(testlabel) + ".csv"
     pridb = pd.read_csv(path)
     #filtered_pridb = di.getPrimaryDatabase(testlabel, 1, True)
@@ -249,11 +325,12 @@ if __name__ == "__main__":
         time_difference = t[hc_index]  # type: ignore
         time_lst[i][0] = time_lst[i][0] + time_difference
     print(time_lst[:, 0])
+    time_array = time_lst[:, 0]
     n_values = np.shape(trai_lst)[0]
-    new_times = np.reshape(time_lst, (int(n_values / n_sensors), n_sensors))
-    print(new_times)
+    new_times = np.reshape(time_array, (int(n_values / n_sensors), n_sensors))
+    print(new_times)"""
 
-    """
+
     # initialise object
     localiser = LS_localiser()
     # call LU_array, rel_LU_array, X_pred, v
@@ -261,4 +338,5 @@ if __name__ == "__main__":
     print(f'array of LU is \n {LU_array}')
     print(f'array of relative LU is \n {rel_LU_array}')
     print(f'wave speed \n {v}')
-    print(f'predicted locations \n {X_pred}')"""
+    print(f'predicted locations \n {X_pred}')
+    localiser.write_to_csv(X_pred, LU_array, 1, "LE", 1)
