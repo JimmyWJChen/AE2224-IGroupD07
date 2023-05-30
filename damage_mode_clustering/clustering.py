17fd27c649897e1430a27737484c3752627d820d
bgrochowski <90771203+bgrochowski@users.noreply.github.com>
2023-05-30 20:35:16 +0200

    CLUSTERS GOOD!

--- damage_mode_clustering/clustering.py
+++ damage_mode_clustering/clustering.py
@@ -67,7 +67,7 @@ if __name__=="__main__":
     label = "PD_PCLSR_QI00"
     filtered = True
     n_clusters = 3
-    params = ['frequency', 'amplitude']
+    params = ['frequency', 'wpfrequency', 'amplitude_db', 'RA', 'rise_time']
     multichannel = False
 
     sum_31 = 0
@@ -75,67 +75,67 @@ if __name__=="__main__":
     sum_211 = 0
     sum_4 = 0
     
-    datapoints = di.getPrimaryDatabase(label, filtered=filtered)
+    datapoints = di.getHitDatabase(label, created=True)
     if not filtered:
         datapoints = di.addPeakFreq(datapoints, label)
         # datapoints = datapoints.read_hits()
-    datapoints = di.addRA(datapoints)
+    # datapoints = di.addRA(datapoints)
     # print(datapoints.head(20))
     hits_total = int(datapoints.max()['hit_id'])
     datapoints['cluster'] = np.zeros(len(datapoints.index))
-    channels = int(datapoints.max()['channel'])
+    # channels = int(datapoints.max()['channel'])
 
-    if multichannel:
-        for ch in range(1, channels+1):
-            print(ch)
-            datapoints_ch = datapoints.loc[datapoints['channel'] == ch].copy()
-            datapoints_ch = clustering_hierarchical(datapoints_ch, n_clusters, params=params)
-            # print(datapoints_ch)
-            for index, row in datapoints_ch.iterrows():
-                datapoints.loc[index, 'cluster'] = row['cluster']
-            # for i in range(n_clusters):
-            #     pridb_cluster = datapoints_ch.loc[datapoints_ch['cluster'] == i]
-            #     plt.scatter(pridb_cluster['amplitude'], pridb_cluster['wpfrequency']/1000, label='Cluster '+str(i+1))
-            # plt.xlabel('Amplitude [dB]')
-            # plt.ylabel('Weighted Peak Frequency [kHz]')
-            # plt.legend()
-            # plt.show()
+    # if multichannel:
+    #     for ch in range(1, channels+1):
+    #         print(ch)
+    #         datapoints_ch = datapoints.loc[datapoints['channel'] == ch].copy()
+    #         datapoints_ch = clustering_hierarchical(datapoints_ch, n_clusters, params=params)
+    #         # print(datapoints_ch)
+    #         for index, row in datapoints_ch.iterrows():
+    #             datapoints.loc[index, 'cluster'] = row['cluster']
+    #         # for i in range(n_clusters):
+    #         #     pridb_cluster = datapoints_ch.loc[datapoints_ch['cluster'] == i]
+    #         #     plt.scatter(pridb_cluster['amplitude'], pridb_cluster['wpfrequency']/1000, label='Cluster '+str(i+1))
+    #         # plt.xlabel('Amplitude [dB]')
+    #         # plt.ylabel('Weighted Peak Frequency [kHz]')
+    #         # plt.legend()
+    #         # plt.show()
 
-    else:
-        datapoints = clustering_kmeans(datapoints, n_clusters, params=params)
+    datapoints = clustering_kmeans(datapoints, n_clusters, params=params)
 
-    print(datapoints.head(20))
+    # print(datapoints.head(20))
 
-    for hit in range(hits_total):
-        hit_points = datapoints.loc[datapoints['hit_id'] == hit].copy()
-        clustercounts = hit_points['cluster'].value_counts().to_frame()
-        if 3 in clustercounts.values:
-            # print(clustercounts)
-            # correct_cluster = clustercounts.index[clustercounts['count'] == 3].to_list()[0]
-            # incorrect_cluster = clustercounts.index[clustercounts['count'] == 1].to_list()[0]
-            # trai_to_correct = hit_points.loc[hit_points['cluster'] == incorrect_cluster]['trai'].to_list()[0]
-            # datapoints.loc[datapoints['trai'] == trai_to_correct, 'cluster'] = correct_cluster
-            # print(f'{trai_to_correct} moved from Cluster {incorrect_cluster} to {correct_cluster}')
-            sum_31+=1
+    # for hit in range(hits_total):
+    #     hit_points = datapoints.loc[datapoints['hit_id'] == hit].copy()
+    #     clustercounts = hit_points['cluster'].value_counts().to_frame()
+    #     if 3 in clustercounts.values:
+    #         # print(clustercounts)
+    #         correct_cluster = clustercounts.index[clustercounts['count'] == 3].to_list()[0]
+    #         incorrect_cluster = clustercounts.index[clustercounts['count'] == 1].to_list()[0]
+    #         trai_to_correct = hit_points.loc[hit_points['cluster'] == incorrect_cluster]['trai'].to_list()[0]
+    #         datapoints.loc[datapoints['trai'] == trai_to_correct, 'cluster'] = correct_cluster
+    #         # print(f'{trai_to_correct} moved from Cluster {incorrect_cluster} to {correct_cluster}')
+    #         sum_31+=1
             
-        elif 2 in clustercounts.values:
-            if 1 in clustercounts.values:
-                sum_211+=1
-            else:
-                sum_22+=1
-        else:
-            sum_4+=1
-    print(f'3 vs 1 clusters: {sum_31}, {sum_31/hits_total * 100}%')
-    print(f'2 vs 2 clusters: {sum_22}, {sum_22/hits_total * 100}%')
-    print(f'2 vs 1 vs 1 clusters: {sum_211}, {sum_211/hits_total * 100}%')
-    print(f'4 vs 0 clusters: {sum_4}, {sum_4/hits_total * 100}%')
+    #     elif 2 in clustercounts.values:
+    #         if 1 in clustercounts.values:
+    #             sum_211+=1
+    #         else:
+    #             sum_22+=1
+    #     else:
+    #         sum_4+=1
+    # print(f'3 vs 1 clusters: {sum_31}, {sum_31/hits_total * 100}%')
+    # print(f'2 vs 2 clusters: {sum_22}, {sum_22/hits_total * 100}%')
+    # print(f'2 vs 1 vs 1 clusters: {sum_211}, {sum_211/hits_total * 100}%')
+    # print(f'4 vs 0 clusters: {sum_4}, {sum_4/hits_total * 100}%')
     # datapoints = di.createHitDataframe(di.getPrimaryDatabase(label, filtered=filtered))
     print(datapoints)
+    datapoints.to_csv("testing_data/4-channels/HITS_" + label + ".csv", index=False)
     # datapoints = clustering_kmeans(datapoints, n_clusters, params=["wpfrequency", "frequency", "freqcentroid", "amplitude1", "amplitude2", "amplitude3", "amplitude4", "RA"])
     for i in range(n_clusters):
         pridb_cluster = datapoints.loc[datapoints['cluster'] == i]
-        plt.scatter(pridb_cluster['amplitude'], pridb_cluster['frequency']/1000, label='Cluster '+str(i+1))
-    plt.xlabel('Amplitude [V]')
+        plt.scatter(pridb_cluster['amplitude_db'], pridb_cluster['frequency']/1000, label='Cluster '+str(i+1))
+    plt.xlabel('Amplitude [dB]')
     plt.ylabel('Peak Frequency [kHz]')
     plt.legend()
     plt.show()
--- data_import.py
+++ data_import.py
@@ -2,8 +2,12 @@ import os
 
 import vallenae as vae
 import pandas as pd
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, ifft
+from scipy import signal
 import numpy as np
+import pywt
+import matplotlib.pyplot as plt
+from filterpy.kalman import KalmanFilter
 
 
 def getPrimaryDatabase(label, testno=1, filtered=False):
@@ -53,11 +57,98 @@ def getWaveform(label, testno=1, trai=1):
         y, t = tradb.read_wave(trai)
     return y, t
 
+def filterWaveform(y, t, trai=1):
+    fs = 400/0.0002
+    cutoff_freq = 20e3  # Cutoff frequency (Hz)
+    order = 4  # Filter order
+
+    # ToA
+    hc_index = vae.timepicker.hinkley(y, alpha=5)[1]
+    y = y[hc_index:]
+    t = t[hc_index:]
+    # print(hc_index)
+    # original_y = y[:]
+    # original_t = t[:]
+    # y = [0 for i in range(hc_index)] + y[hc_index:]
+    # noise_y = y[:hc_index] + [0 for i in range(n - hc_index)]
+
+    # fft_y = fft(np.concatenate((np.zeros(hc_index), y)))
+    # fft_noise_y = fft(np.concatenate((noise_y, np.zeros(n - hc_index))))
+
+    # filtered_signal = ifft(fft_y - fft_noise_y)[hc_index:]
+
+    # Create the Butterworth low-pass filter
+    b, a = signal.butter(order, cutoff_freq, fs=fs, btype='high')
+
+    # Apply the filter to the signal
+    filtered_signal = signal.lfilter(b, a, y)
+
+
+    wavelet = 'db32'
+    level = 4
+
+    # Perform wavelet packet decomposition
+    wp = pywt.WaveletPacket(data=filtered_signal, wavelet=wavelet, mode='symmetric', maxlevel=level)
+
+    # Apply thresholding to coefficients
+    threshold = 0.1 * np.max(np.abs([node.data for node in wp.get_level(level, 'natural')]))
+    for node in wp.get_level(level, 'natural'):
+        node.data[np.abs(node.data) < threshold] = 0
+    reconstructed_data = wp.reconstruct(update=False)
+    # getPeakFrequency(y, t, trai)
+    # plt.figure()
+    # plt.plot(t, y, label='Original Signal')
+    # plt.plot(t, filtered_signal, label='Highpassed Signal')
+    # plt.plot(t, reconstructed_data, label='Filtered Signal')
+    # plt.title(f'Trai: {trai}')
+    # plt.xlabel('Time (s)')
+    # plt.ylabel('Amplitude')
+    # plt.legend()
+    # plt.show()
+    # getPeakFrequency(reconstructed_data, t, trai)
+    return reconstructed_data, t
+
+def mergeSignals(signals, process_noise_variance=1, measurement_noise_variance=1):
+    l = len(signals)
+    kf = KalmanFilter(dim_x=l, dim_z=l)
+    initial_state = np.zeros(l)
+    initial_covariance = np.eye(l)
+    kf.F = np.eye(l)
+    kf.H = np.eye(l)
+    kf.Q = np.eye(l) * process_noise_variance
+    kf.R = np.eye(l) * measurement_noise_variance
+
+    max_length = max(len(signal) for signal in signals)
+
+    # Iterate over each measurement
+    merged_signal = []
+    for t in range(max_length):
+        # Prepare the measurement vector
+        measurement = []
+        for signal in signals:
+            if t < len(signal):
+                measurement.append(signal[t])
+            else:
+                measurement.append(np.nan)  # Placeholder for missing measurements
+
+        # Perform the Kalman filter prediction step
+        kf.predict()
+
+        # Reshape the measurement to match the expected shape
+        measurement = np.reshape(measurement, (l, 1))
+
+        # Perform the Kalman filter update step with the current measurement
+        kf.update(measurement)
+
+        # Append the estimated state (or any desired output) to the merged signal
+        merged_signal.append(kf.x[0])  # Assuming the desired output is the first element of the state vector
+
+    return merged_signal
+
+
+
 
 def getPeakFrequency(y, t, trai=0):
-    idx = np.argmin(abs(t))
-    y = y[idx:]
-    t = t[idx:]
     N = len(y)
     T = t[1] - t[0]
     yf = np.abs(fft(y))
@@ -68,11 +159,34 @@ def getPeakFrequency(y, t, trai=0):
         Fcentr += xf[i] * yf[i]
     Fcentr /= np.sum(yf)
     WPF = np.sqrt(PeakFreq * Fcentr)
-    # import matplotlib.pyplot as plt
     # plt.plot(xf, yf[:N//2])
+    # # plt.plot(t, y)
     # plt.title(f"Trai: {trai}, Peak: {PeakFreq}, WPeak: {WPF}")
     # plt.show()
     return PeakFreq, Fcentr, WPF
+
+def plotScalogram(y, t, trai=0):
+    wavelet = 'db5'  # Example wavelet (replace with your choice)
+    level = 5  # Example decomposition level (replace with your choice)
+    coefficients = pywt.wavedec(y, wavelet, level=level)
+
+    scalogram = []
+    max_len = max(len(coeff) for coeff in coefficients)
+
+    for coeff in coefficients:
+        coeff_len = len(coeff)
+        padding = max_len - coeff_len
+        coeff = np.pad(coeff, (0, padding), mode='constant')
+        scalogram.append(np.abs(coeff)**2)
+
+    scalogram = np.array(scalogram, dtype=float)
+
+    plt.imshow(scalogram, cmap='hot', aspect='auto')
+    plt.title(f'Trai: {trai}')
+    plt.colorbar()
+    plt.show()
+
+
 
 def addPeakFreq(pridb, label, testno=1):
     try:
@@ -88,6 +202,7 @@ def addPeakFreq(pridb, label, testno=1):
     for trai in trais:
         if trai%1000==0: print(trai)
         y, t = tradb.read_wave(trai)
+        y, t = filterWaveform(y, t)
         f, fcentr, wpf = getPeakFrequency(y, t, trai)
         frequencies.append(f)
         fcentrs.append(fcentr)
@@ -98,7 +213,7 @@ def addPeakFreq(pridb, label, testno=1):
     return pridb
 
 def addDecibels(pridb):
-    base = np.min(pridb["amplitude"])
+    base = 10e-6
     pridb["amplitude_db"] = 20 * np.log10(pridb["amplitude"]/base)
     return pridb
 
@@ -106,19 +221,33 @@ def addRA(pridb):
     pridb["RA"] = pridb["rise_time"]/pridb["amplitude"]
     return pridb
 
-def createHitDataframe(pridb):
-    hitdb = pd.DataFrame(columns=["hit_id", "time", "wpfrequency", "frequency", "freqcentroid", "amplitude1", "amplitude2", "amplitude3", "amplitude4", "RA"])
+def createHitDataframe(pridb, label, testno=1):
+    hitdb = pd.DataFrame(columns=["hit_id", "time", "wpfrequency", "frequency", "freqcentroid", "amplitude", "rise_time"])
     hits_total = int(pridb.max()['hit_id'])
+    tradb = getTransientDatabase(label, testno)
     for hit in range(hits_total):
+        if hit%10==0:
+            print(hit)
         hit_points = pridb.loc[pridb['hit_id'] == hit].copy()
         time = hit_points['time'].mean()
-        wpfrequency = hit_points['wpfrequency'].mean()
-        frequency = hit_points['frequency'].mean()
-        freqcentroid = hit_points['freqcentroid'].mean()
-        ra = hit_points['RA'].mean()
+        trais = hit_points['trai']
+        signals = []
+        for trai in trais:
+            y, t = tradb.read_wave(trai)
+            y, t = filterWaveform(y, t)
+            signals.append(y)
+        merged_y = np.transpose(mergeSignals(signals))[0]
+        merged_y = merged_y[~np.isnan(merged_y)]
+        merged_t = np.arange(0, (len(merged_y)) * 0.0002/400, 0.0002/400)
+        if len(merged_t) != len(merged_y):
+            min_len = min(len(merged_t), len(merged_y))
+            merged_t = merged_t[:min_len]
+            merged_y = merged_y[:min_len]
+        PeakFreq, Fcentr, WPF = getPeakFrequency(merged_y, merged_t)
         hit_points.sort_values(by=['time'], inplace=True)
-        hitdb.loc[len(hitdb.index)] = [hit, time, wpfrequency, frequency, freqcentroid,
-                                       hit_points['amplitude'].iloc[0], hit_points['amplitude'].iloc[1], hit_points['amplitude'].iloc[1], hit_points['amplitude'].iloc[3], ra]
+        amplitude = (np.max(merged_y) - np.min(merged_y))/2
+        rise_time = vae.features.rise_time(merged_y, 0.1 * amplitude, 400/0.0002)
+        hitdb.loc[len(hitdb.index)] = [hit, time, WPF, PeakFreq, Fcentr, amplitude, rise_time]
     return hitdb
 
 def filterPrimaryDatabase(pridb, label, testno=1, sortby="energy", epsilon=0.2, epsilon_mc=0.001, freqmargin = 5, thamp=0.009, thdur = 0.002, thenergy=1e5, thstrength=2500, thcounts=70, saveToCSV=False):
@@ -262,14 +391,54 @@ def getHitsPerSensor(pridb):
     hitsno = [len(pridb.loc[pridb['channel'] == i]) for i in range(1, int(pridb.max()['channel'])+1)]
     return hitsno
 
+def getHitDatabase(label, created=True):
+    if created:
+        path = "testing_data/4-channels/HITS_" + label + ".csv"
+        hitdb = pd.read_csv(path)
+        return hitdb
+    else:
+        hitdb = createHitDataframe(getPrimaryDatabase(label, filtered=True), label)
+        hitdb = addRA(hitdb)
+        hitdb = addDecibels(hitdb)
+        hitdb.to_csv("testing_data/4-channels/HITS_" + label + ".csv", index=False)
+        return hitdb
+
 
 if __name__ == "__main__":
     testlabel = "PD_PCLO_QI090"
     testno = 1
-    pridb = getPrimaryDatabase(testlabel, testno, filtered=False)
-    pridb = addPeakFreq(pridb, testlabel)
-    pridb = addRA(pridb)
-    print(pridb[:10])
+    pridb = getPrimaryDatabase(testlabel, testno, filtered=True)
+    # pridb = addPeakFreq(pridb, testlabel)
+    # pridb = addRA(pridb)
+    hitdb = createHitDataframe(pridb, testlabel)
+    hitdb = addRA(hitdb)
+    hitdb = addDecibels(hitdb)
+    print(hitdb)
+    hitdb.to_csv("testing_data/4-channels/HITS_" + testlabel + ".csv", index=False)
+    
+    # print(pridb[:15])
+    # signals = []
+    # for i in range(32):
+    #     trai = pridb['trai'][i]
+    #     y, t = getWaveform(testlabel, trai=trai)
+    #     # print(t[-1])
+    #     y, t = filterWaveform(y, t, trai)
+    #     # print(t[-1])
+    #     signals.append(y)
+    #     # plt.plot(t, y)
+    #     # plt.title(f'Trai = {trai}')
+    #     # plt.show()
+    #     if len(signals) == 4:
+    #         merged_y = np.transpose(mergeSignals(signals))[0]
+    #         merged_y = merged_y[~np.isnan(merged_y)]
+    #         merged_t = np.arange(0, (len(merged_y)) * 0.0002/400, 0.0002/400)
+    #         if len(merged_t) != len(merged_y):
+    #             min_len = min(len(merged_t), len(merged_y))
+    #             merged_t = merged_t[:min_len]
+    #             merged_y = merged_y[:min_len]
+    #         plt.plot(merged_t, merged_y)
+    #         plt.show()
+    #         signals = []
     # pridb = filterPrimaryDatabase(pridb, testlabel)
     # print(pridb)
     # hitdb = createHitDataframe(pridb)
